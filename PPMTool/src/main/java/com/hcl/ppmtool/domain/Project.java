d0e296ce0bdc5ffaabd28cca1dcf7814492ee204
Imad Mashhood <mashhoodimad@gmail.com>
2022-01-18 19:13:37 -0600

    Setup JSON reqs and formats

--- PPMTool/src/main/java/com/hcl/ppmtool/domain/Project.java
+++ PPMTool/src/main/java/com/hcl/ppmtool/domain/Project.java
@@ -1,6 +1,10 @@
 package com.hcl.ppmtool.domain;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+
 import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
 import java.util.Date;
 
 @Entity
@@ -8,13 +12,21 @@ public class Project {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @NotBlank(message = "Project Name Required")
     private String projectName;
+    @NotBlank(message = "Project ID Required")
+    @Size(min = 4, max = 5, message = "Project ID has to be between 4-5 characters")
+    @Column(updatable = false, unique = true)
     private String projectIdentifier;
+    @NotBlank(message = "Project Description Required")
     private String description;
+    @JsonFormat(pattern = "yyyy-mm-dd")
     private Date start_date;
+    @JsonFormat(pattern = "yyyy-mm-dd")
     private Date end_date;
-
+    @JsonFormat(pattern = "yyyy-mm-dd")
     private Date created_At;
+    @JsonFormat(pattern = "yyyy-mm-dd")
     private Date updated_At;
 
     public Project(){
