518ca506fb5c96717ee062c8c9a298b0b57b5f68
Imad Mashhood <mashhoodimad@gmail.com>
2022-01-18 21:21:08 -0600

    Refactor error validation and clean up code in the controller

--- PPMTool/src/main/java/com/hcl/ppmtool/web/ProjectController.java
+++ PPMTool/src/main/java/com/hcl/ppmtool/web/ProjectController.java
@@ -1,6 +1,7 @@
 package com.hcl.ppmtool.web;
 
 import com.hcl.ppmtool.domain.Project;
+import com.hcl.ppmtool.services.MapValidationErrorService;
 import com.hcl.ppmtool.services.ProjectService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -25,15 +26,13 @@ public class ProjectController {
     @Autowired
     private ProjectService projectService;
 
+    @Autowired
+    private MapValidationErrorService mapValidationErrorService;
+
     @PostMapping("")
     public ResponseEntity<?> createNewProject(@Valid @RequestBody Project project, BindingResult result){
-        if(result.hasErrors()){
-            Map<String, String> errorMap = new HashMap<>();
-            for(FieldError error: result.getFieldErrors()){
-                errorMap.put(error.getField(), error.getDefaultMessage());
-            }
-            return new ResponseEntity<Map<String, String>>(errorMap, HttpStatus.BAD_REQUEST);
-        }
+        ResponseEntity<?> errorMap = mapValidationErrorService.MapValidationService(result);
+        if(errorMap!=null) return errorMap;
         Project project1 = projectService.saveOrUpdate(project);
         return new ResponseEntity<Project>(project1, HttpStatus.CREATED);
     }
