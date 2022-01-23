1f47fce21ab89b99c4f3f723aab1004e6040ec51
Imad Mashhood <mashhoodimad@gmail.com>
2022-01-22 19:35:07 -0600

    Setup get all fo redux

--- ppmtool-react-client/src/reducers/index.js
+++ ppmtool-react-client/src/reducers/index.js
@@ -1,6 +1,8 @@
 import { combineReducers } from "redux";
 import errorReducer from "./errorReducer.js";
+import ProjectReducer from "./ProjectReducer.js";
 
 export default combineReducers({
   errors: errorReducer,
+  project: ProjectReducer,
 });
