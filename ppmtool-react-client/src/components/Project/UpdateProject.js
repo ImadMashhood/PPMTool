d9e24e1751f3b66e0ced8f24d98986ed9d6aaa10
Imad Mashhood <mashhoodimad@gmail.com>
2022-01-24 14:13:25 -0600

    WIP Fix for getProject

--- ppmtool-react-client/src/components/Project/UpdateProject.js
+++ ppmtool-react-client/src/components/Project/UpdateProject.js
@@ -1,75 +1,91 @@
 import React, { Component } from "react";
-import { getProject } from "../../actions/projectActions";
 import PropTypes from "prop-types";
 import { connect } from "react-redux";
+import { getProject } from "../../actions/projectActions";
 import classnames from "classnames";
+import { motion } from "framer-motion";
 
 class UpdateProject extends Component {
-  //set state
   componentDidMount() {
     const { id } = this.props.match.params;
     this.props.getProject(id, this.props.history);
   }
   render() {
     return (
-      <div className="project">
-        <div className="container">
-          <div className="row">
-            <div className="col-md-8 m-auto">
-              <h5 className="display-4 text-center">Update Project form</h5>
-              <hr />
-              <form>
-                <div className="form-group">
+      <motion.div
+        initial={{ opacity: 0, y: -100 }}
+        animate={{ opacity: 1, y: 0 }}
+        exit={{ opacity: 0, y: 100 }}
+      >
+        <div className="project">
+          <div className="container">
+            <div className="row">
+              <div className="col-md-8 m-auto">
+                <h5 className="display-4 text-center">Update Project Form</h5>
+                <hr />
+                <form className="form-main" onSubmit={this.onSubmit}>
+                  <div
+                    className="form-group"
+                    style={{ "margin-bottom": "20px" }}
+                  >
+                    <input
+                      type="text"
+                      className={classnames("form-control form-control-lg ")}
+                      placeholder="Project Name"
+                      name="projectName"
+                    />
+                  </div>
+                  <div
+                    className="form-group"
+                    style={{ "margin-bottom": "20px" }}
+                  >
+                    <input
+                      type="text"
+                      className={classnames("form-control form-control-lg ")}
+                      placeholder="Unique Project ID"
+                      name="projectIdentifier"
+                    />
+                  </div>
+                  <div
+                    className="form-group"
+                    style={{ "margin-bottom": "20px" }}
+                  >
+                    <textarea
+                      className={classnames("form-control form-control-lg ")}
+                      placeholder="Project Description"
+                      name="description"
+                    ></textarea>
+                  </div>
+                  <h6 style={{ color: "#fff" }}>Start Date</h6>
+                  <div
+                    className="form-group"
+                    style={{ "margin-bottom": "20px" }}
+                  >
+                    <input
+                      type="date"
+                      className={classnames("form-control form-control-lg ")}
+                      name="start_date"
+                    />
+                  </div>
+                  <h6 style={{ color: "#fff" }}>Estimated End Date</h6>
+                  <div className="form-group">
+                    <input
+                      type="date"
+                      className={classnames("form-control form-control-lg ")}
+                      name="end_date"
+                    />
+                  </div>
                   <input
-                    type="text"
-                    className="form-control form-control-lg "
-                    placeholder="Project Name"
-                    name="projectName"
-                    value={this.state.projectName}
+                    type="submit"
+                    className="btn-primary"
+                    style={{ "margin-bottom": "20px" }}
                   />
-                </div>
-                <div className="form-group">
-                  <input
-                    type="text"
-                    className="form-control form-control-lg"
-                    placeholder="Unique Project ID"
-                    name="projectIdentifier"
-                    disabled
-                  />
-                </div>
-                <div className="form-group">
-                  <textarea
-                    className="form-control form-control-lg"
-                    placeholder="Project Description"
-                    name="description"
-                  />
-                </div>
-                <h6>Start Date</h6>
-                <div className="form-group">
-                  <input
-                    type="date"
-                    className="form-control form-control-lg"
-                    name="start_date"
-                  />
-                </div>
-                <h6>Estimated End Date</h6>
-                <div className="form-group">
-                  <input
-                    type="date"
-                    className="form-control form-control-lg"
-                    name="end_date"
-                  />
-                </div>
-
-                <input
-                  type="submit"
-                  className="btn btn-primary btn-block mt-4"
-                />
-              </form>
+                </form>
+              </div>
             </div>
           </div>
         </div>
-      </div>
+      </motion.div>
     );
   }
 }
