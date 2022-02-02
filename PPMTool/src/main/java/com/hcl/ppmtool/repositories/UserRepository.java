a890fb64625b77929a5996c099288ad917fc8897
Jason Dinh <dinhjdev@gmail.com>
2022-02-02 00:22:16 -0600

    JWT Provider

--- PPMTool/src/main/java/com/hcl/ppmtool/repositories/UserRepository.java
+++ PPMTool/src/main/java/com/hcl/ppmtool/repositories/UserRepository.java
@@ -1,5 +1,7 @@
 package com.hcl.ppmtool.repositories;
 
+import java.util.Optional;
+
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,5 +9,6 @@ import com.hcl.ppmtool.domain.User;
 
 @Repository
 public interface UserRepository extends CrudRepository<User, Long> {
-
+	User findByUsername(String username);
+	User getById(Long id);
 }
