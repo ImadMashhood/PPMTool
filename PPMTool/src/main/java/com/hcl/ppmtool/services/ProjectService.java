91ef0cc30ed2641a9609ad0d26f61580c68b08b9
Jason Dinh <dinhjdev@gmail.com>
2022-02-02 01:54:38 -0600

    Lock Updates to specific User

--- PPMTool/src/main/java/com/hcl/ppmtool/services/ProjectService.java
+++ PPMTool/src/main/java/com/hcl/ppmtool/services/ProjectService.java
@@ -26,8 +26,18 @@ public class ProjectService {
 
     //Used for creating projects
     public Project saveOrUpdate(Project project, String username){
+    	
+    	if(project.getId() != null) {
+    		Project existingProject = projectRepository.findByProjectIdentifier(project.getProjectIdentifier());
+    		
+    		if(existingProject !=null &&(!existingProject.getProjectLeader().equals(username))) {
+    			throw new ProjectNotFoundException("Project not found in your account");
+    		} else if(existingProject == null) {
+    			throw new ProjectNotFoundException("Project with ID: '" + project.getProjectIdentifier()+"' cannot be updated because it doesn't exist");
+    		}
+    	}
+    	
         try{
-        	
         	User user = userRepository.findByUsername(username);
         	project.setUser(user);
         	project.setProjectLeader(user.getUsername());        	
