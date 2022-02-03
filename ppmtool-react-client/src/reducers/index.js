60935fdb1a1355f1fa68ad4f2b29446e0ef59c7d
Imad Mashhood <mashhoodimad@gmail.com>
2022-02-03 14:26:44 -0600

    Set up reducedrs

--- ppmtool-react-client/src/reducers/index.js
+++ ppmtool-react-client/src/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from "redux";
 import errorReducer from "./errorReducer.js";
 import ProjectReducer from "./ProjectReducer.js";
+import securityReducer from "./securityReducer";
 
 export default combineReducers({
   errors: errorReducer,
   project: ProjectReducer,
+  security: securityReducer,
 });
