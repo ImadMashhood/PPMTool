eb6e5a9b7474f717131bacb025b21f66094ef71d
Imad Mashhood <mashhoodimad@gmail.com>
2022-02-03 20:30:59 -0600

    Fix login setup

--- ppmtool-react-client/src/App.js
+++ ppmtool-react-client/src/App.js
@@ -15,6 +15,7 @@ import signupForm from "./components/accountBox/signupForm";
 import jwt_decode from "jwt-decode";
 import setJWTToken from "./securityUtils/setJWTToken";
 import { SET_CURRENT_USER } from "./actions/types";
+import { logout } from "./actions/securityActions";
 
 const jwtToken = localStorage.jwtToken;
 
@@ -28,8 +29,8 @@ if (jwtToken) {
 
   const currentTime = Date.now() / 1000;
   if (decoded_jwtToken.exp < currentTime) {
-    //handle logout
-    //window.location.href = "/";
+    store.dispatch(logout());
+    window.location.href = "/";
   }
 }
 
--- ppmtool-react-client/src/components/accountBox/loginForm.js
+++ ppmtool-react-client/src/components/accountBox/loginForm.js
@@ -10,6 +10,7 @@ class Login extends Component {
     this.state = {
       username: "",
       password: "",
+      errors: {},
     };
     this.onChange = this.onChange.bind(this);
     this.onSubmit = this.onSubmit.bind(this);
@@ -18,6 +19,10 @@ class Login extends Component {
   componentWillReceiveProps(nextProps) {
     if (nextProps.security.validToken) {
       this.props.history.push("/dashboard");
+    }
+
+    if (nextProps.errors) {
+      this.setState({ errors: nextProps.errors });
     }
   }
 
@@ -36,6 +41,7 @@ class Login extends Component {
   }
 
   render() {
+    const { errors } = this.state;
     return (
       <div className="login">
         <div className="container">
@@ -43,31 +49,39 @@ class Login extends Component {
             <div className="col-md-8 m-auto">
               <h1 className="display-4 text-center">Log In</h1>
               <form onSubmit={this.onSubmit}>
-                <div className="form-group" style={{ "margin-bottom": "20px" }}>
+                <div className="form-group">
                   <input
                     type="text"
-                    className="form-control form-control-lg"
+                    className={classnames("form-control form-control-lg", {
+                      "is-invalid": errors.username,
+                    })}
+                    style={{ "margin-bottom": "20px" }}
                     placeholder="Email Address"
                     name="username"
                     value={this.state.username}
                     onChange={this.onChange}
                   />
+                  {errors.username && (
+                    <div className="invalid-feedback">{errors.username}</div>
+                  )}
                 </div>
                 <div className="form-group">
                   <input
                     type="password"
-                    className="form-control form-control-lg"
+                    className={classnames("form-control form-control-lg", {
+                      "is-invalid": errors.password,
+                    })}
+                    style={{ "margin-bottom": "20px" }}
                     placeholder="Password"
                     name="password"
                     value={this.state.password}
                     onChange={this.onChange}
                   />
+                  {errors.password && (
+                    <div className="invalid-feedback">{errors.password}</div>
+                  )}
                 </div>
-                <input
-                  type="submit"
-                  className="btn btn-info btn-block mt-4"
-                  style={{ "margin-bottom": "20px" }}
-                />
+                <input type="submit" className="btn btn-info btn-block mt-4" />
               </form>
             </div>
           </div>
@@ -80,6 +94,7 @@ class Login extends Component {
 Login.propTypes = {
   login: PropTypes.func.isRequired,
   errors: PropTypes.object.isRequired,
+  security: PropTypes.object.isRequired,
 };
 
 const mapStateToProps = (state) => ({
