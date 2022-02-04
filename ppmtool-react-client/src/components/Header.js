e2ad60d6499bb2403aa4f4f174299811584b35e4
Imad Mashhood <mashhoodimad@gmail.com>
2022-02-03 22:08:15 -0600

    finalize header logic

--- ppmtool-react-client/src/components/Header.js
+++ ppmtool-react-client/src/components/Header.js
@@ -9,33 +9,71 @@ import {
   NavImg,
 } from "./Layout/HeaderElements";
 import { ReactComponent as ImgLogo } from "../images/Logo.svg";
+import PropTypes from "prop-types";
+import { connect } from "react-redux";
+import { logout } from "../actions/securityActions";
 
 class Header extends Component {
+  logout() {
+    this.props.logout();
+    window.location.href = "/";
+  }
   render() {
+    const { validToken, user } = this.props.security;
+    const userIsNotAuthenticated = (
+      <NavMenu>
+        <NavLink to="/about-us" activestyle>
+          About Us
+        </NavLink>
+        <NavLink to="/signup" activestyle>
+          Sign Up
+        </NavLink>
+        <NavBtnLink to="/signin">Sign In</NavBtnLink>
+      </NavMenu>
+    );
+    const userIsAuthenticated = (
+      <NavMenu>
+        <NavLink to="/about-us" activestyle>
+          About Us
+        </NavLink>
+        <NavLink to="/dashboard" activestyle>
+          {user.fullName}
+        </NavLink>
+        <NavLink to="/logout" activestyle onClick={this.logout.bind(this)}>
+          Logout
+        </NavLink>
+      </NavMenu>
+    );
+
+    let headerLinks;
+
+    if (validToken && user) {
+      headerLinks = userIsAuthenticated;
+    } else {
+      headerLinks = userIsNotAuthenticated;
+    }
+
     return (
       <>
-        {/*Custom Nav button written using this video as a guide: https://www.youtube.com/watch?v=VzWBLj_CfpE*/}
         <Nav>
           <NavImg to="/">
             <ImgLogo />
           </NavImg>
           <Bars />
-          <NavMenu>
-            <NavLink to="/dashboard" activestyle>
-              Dashboard
-            </NavLink>
-            <NavLink to="/about-us" activestyle>
-              About Us
-            </NavLink>
-            <NavLink to="/signup" activestyle>
-              Sign Up
-            </NavLink>
-            <NavBtnLink to="/signin">Sign In</NavBtnLink>
-          </NavMenu>
+          <NavMenu>{headerLinks}</NavMenu>
         </Nav>
       </>
     );
   }
 }
 
-export default Header;
+Header.propTypes = {
+  logout: PropTypes.func.isRequired,
+  security: PropTypes.object.isRequired,
+};
+
+const mapStateToProps = (state) => ({
+  security: state.security,
+});
+
+export default connect(mapStateToProps, { logout })(Header);
